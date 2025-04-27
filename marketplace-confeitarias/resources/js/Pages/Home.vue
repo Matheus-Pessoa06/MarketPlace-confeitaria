81ee3aa4de6c1677763fecf5bc81b2982c3c02f9
Matheus <matheuspessoa2010@gmail.com>
2025-04-27 18:58:12 -0300

    innitial commit

---
+++ marketplace-confeitarias/resources/js/Pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="p-6">
+      <h1 class="text-2xl font-bold">Bem-vindo ao Marketplace de Confeitarias!</h1>
+      <p class="mt-4">Tudo está funcionando! 🎉</p>
+    </div>
+  </template>
+  
